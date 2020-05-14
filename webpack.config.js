5ec6472ba631fe9010bfe29b1f873707d45a7489
Zaid Bhura <3915682+asdeev@users.noreply.github.com>
2020-05-14 12:45:55 -0700

    Works for prod builds, WIP for styleguidist

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,98 @@
-const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractCSSChunksPlugin = require('extract-css-chunks-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
+const webpack = require('webpack');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+
+const isProd = process.env.NODE_ENV !== 'production';
 
 module.exports = {
   entry: './src/index.tsx',
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, 'src', 'index.html'),
-    }),
-    new CleanWebpackPlugin({
-      dry: false,
-      verbose: true,
-      cleanStaleWebpackAssets: true,
-      protectWebpackAssets: true,
-    }),
-  ],
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+    },
+    minimize: true,
+    minimizer: [
+      new webpack.DefinePlugin({
+        NODE_ENV: process.env.NODE_ENV || 'development',
+        PUBLIC_URL: '/path/to/public/dir',
+      }),
+      new HtmlWebpackPlugin(
+        Object.assign(
+          {},
+          {
+            inject: true,
+            template: path.resolve(__dirname, 'src', 'index.html'),
+          },
+          isProd
+            ? {
+                minify: {
+                  removeComments: true,
+                  collapseWhitespace: true,
+                  removeRedundantAttributes: true,
+                  useShortDoctype: true,
+                  removeEmptyAttributes: true,
+                  removeStyleLinkTypeAttributes: true,
+                  keepClosingSlash: true,
+                  minifyJS: true,
+                  minifyCSS: true,
+                  minifyURLs: true,
+                },
+              }
+            : undefined
+        )
+      ),
+      new CleanWebpackPlugin({
+        dry: false,
+        verbose: true,
+        cleanStaleWebpackAssets: true,
+        protectWebpackAssets: true,
+      }),
+      new TerserPlugin({
+        terserOptions: {
+          parse: {
+            // Terser needs to parse ECMA 8 code.
+            // However it shouldn't apply minifaction that will turn valid ECMA 5 code
+            // into invalid ECMA 5 code. The 'compress' and 'output' property help with it.
+            ecma: 8,
+          },
+          compress: {
+            ecma: 5,
+            warnings: false,
+            comparisons: false,
+            inline: 2,
+          },
+          mangle: {
+            safari10: true,
+          },
+          output: {
+            ecma: 5,
+            comments: false,
+            // eslint-disable-next-line @typescript-eslint/camelcase
+            ascii_only: true,
+          },
+        },
+        parallel: true, // use multi-process parallel running for faster build speed
+        cache: true, // enable file caching,
+        sourceMap: true, // must be set to true if using source-maps in production
+      }),
+      new OptimizeCSSAssetsPlugin({
+        cssProcessorOptions: {
+          map: {
+            inline: false,
+            annotation: true,
+          },
+        },
+      }),
+      new ExtractCSSChunksPlugin({
+        filename: isProd ? '[name].[hash].css' : '[name].css',
+        chunkFilename: isProd ? '[id].[hash].css' : '[id].css',
+      }),
+    ],
+  },
   module: {
     rules: [
       {
@@ -22,16 +100,26 @@ module.exports = {
         use: 'html-loader',
       },
       {
-        test: /\.css$/,
+        test: /\.(sa|sc|c)ss$/,
         include: /node_modules/,
-        use: ['style-loader', 'css-loader'],
+        use: [
+          {
+            loader: ExtractCSSChunksPlugin.loader,
+            options: {
+              public: '/styles',
+              hmr: process.env.NODE_ENV === 'development',
+            },
+          },
+          'css-loader',
+          'sass-loader',
+        ],
       },
       {
         test: /\.scss$/,
         use: ['style-loader', 'css-loader', 'sass-loader'],
       },
       {
-        test: /\.(js|ts|tsx)$/,
+        test: /\.(js|ts|jsx|tsx)$/,
         exclude: /node_modules/,
         use: 'babel-loader',
       },
@@ -39,7 +127,16 @@ module.exports = {
   },
   resolve: {
     descriptionFiles: ['package.json'],
-    extensions: ['.js', '.ts', '.tsx', '.scss', '.css', '.html', '.json'],
+    extensions: [
+      '.js',
+      '.ts',
+      '.jsx',
+      '.tsx',
+      '.scss',
+      '.css',
+      '.html',
+      '.json',
+    ],
     alias: {
       styles: path.resolve(__dirname, 'styles/'),
       components: path.resolve(__dirname, 'src/components/'),
@@ -49,9 +146,11 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.[hash].js',
+    filename: '[name].[hash:8].js',
+    sourceMapFilename: '[name].[hash:8].map',
+    chunkFilename: '[id].[hash:8].js',
   },
-  devtool: 'hidden-source-map',
+  devtool: !isProd ? 'cheap-module-eval-source-map' : false,
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
     compress: true,
