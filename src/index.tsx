3791ede45b68a0c0bc295ab30c3959d787164fb7
Zaid Bhura <3915682+asdeev@users.noreply.github.com>
2020-09-27 18:49:28 -0700

    Restructured for atomic design

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faInfoCircle } from '@fortawesome/free-solid-svg-icons/faInfoCircle';
 
-import App from 'components/App/App';
+import App from 'components/App';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'styles/main';
